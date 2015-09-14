9be9921e9fd3b825ee940605ca6bb0d845409de6
Jonathan Thomas <thomas.jonathan.c@gmail.com>
2015-09-13 23:03:57 -0400

    It technically works
    But more work is needed. I'm going to change the way block comments are
    stored.

--- src/linecounter/userinterface/LanguageSelectionPanel.java
+++ src/linecounter/userinterface/LanguageSelectionPanel.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.border.TitledBorder;
 
@@ -22,7 +23,7 @@ import linecounter.Settings;
 public class LanguageSelectionPanel extends SourceCounterPanel {
 	
 	private JComboBox<?> languagesDropDown;
-	private JTable detailsTable;
+	private JScrollPane detailsPane;
 	private JButton newLangButton;
 	
 	public LanguageSelectionPanel(Settings set) {
@@ -34,7 +35,7 @@ public class LanguageSelectionPanel extends SourceCounterPanel {
 		languagesDropDown  = new JComboBox<Object>(settings.availableLangs.availableLangs());
         languagesDropDown.setSelectedIndex(settings.selectedLangIndex);
         
-        detailsTable = new JTable();
+        detailsPane = populateTable();
         
         newLangButton = new JButton("Define New Language");
         
@@ -46,9 +47,52 @@ public class LanguageSelectionPanel extends SourceCounterPanel {
         });
         
         add(languagesDropDown);
-        add(detailsTable);
+        add(detailsPane);
         add(newLangButton);
         setBorder(new TitledBorder("Language"));
+	}
+	
+	private JScrollPane populateTable() {
+		String[] columnNames = {"Extensions", "Comment Characters"};
+		
+		// Decide how many rows the table should have. The comment character column will never have
+		// more than 3 items (conceivably), so if there are 3 or fewer
+		int extensionCount = settings.selectedLang().extensions.length;
+		int commentCharCount = (int) settings.selectedLang().lineCommentChars.length + 
+				(settings.selectedLang().blockCommentDelimiters.length / 2);
+		Object[][] data = new Object[(extensionCount>commentCharCount? extensionCount : commentCharCount)][2];
+		
+		for(int i = 0; i < data.length; i++) {
+			if(i<settings.selectedLang().extensions.length) {
+				data[i][0] = settings.selectedLang().extensions[i].toString();
+			} else {
+				data[i][0] = "";
+			}
+			
+			switch(i) {
+			case 0:
+				if(i<settings.selectedLang().lineCommentChars.length) {
+					data[i][1] = settings.selectedLang().lineCommentChars[i];
+				}
+				break;
+			case 1:
+				if(settings.selectedLang().blockCommentDelimiters.length > 0) {
+					data[i][1] = settings.selectedLang().blockCommentDelimiters[0] + " ... "
+							+ settings.selectedLang().blockCommentDelimiters[1];
+				}
+				break;
+			case 2:
+				if(i<settings.selectedLang().lineCommentChars.length) {
+					data[i][1] = settings.selectedLang().lineCommentChars[i];
+				}
+				break;
+			default:
+				data[i][1] = "";
+			}
+			
+			
+		}
+		return new JScrollPane(new JTable(data, columnNames));
 	}
 
 	@Override
