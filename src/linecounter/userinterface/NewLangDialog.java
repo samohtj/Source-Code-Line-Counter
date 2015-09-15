7b0b7e4bc27efb8e037eea60879abd5e19d61492
Jonathan Thomas <thomas.jonathan.c@gmail.com>
2015-09-15 16:35:16 -0400

    Dialog finished
    Time to test it.

--- src/linecounter/userinterface/NewLangDialog.java
+++ src/linecounter/userinterface/NewLangDialog.java
@@ -1,14 +1,23 @@
 package linecounter.userinterface;
 
 import java.awt.GridLayout;
+import java.util.Arrays;
 
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+import linecounter.Language;
 import linecounter.Settings;
 
+/**
+ * A small dialog box that will get all the information needed to add a new language.
+ * @author Jonathan Thomas
+ *
+ */
+// Suppress an "unused" warning on the Arrays import, in case we ever need the debug output again.
+@SuppressWarnings("unused")
 public class NewLangDialog {
 
 	public static void showDialog(Settings settings) {
@@ -17,21 +26,39 @@ public class NewLangDialog {
 		JTextField lineComField = new JTextField(10);
 		JTextField blockComField = new JTextField(10);
 
-		JPanel myPanel = new JPanel(new GridLayout(4, 2));
-		myPanel.add(new JLabel("Name:"));
-		myPanel.add(nameField);
-		myPanel.add(new JLabel("Extensions:"));
-		myPanel.add(extensionsField);
-		myPanel.add(new JLabel("Line comment characters:"));
-		myPanel.add(lineComField);
-		myPanel.add(new JLabel("Block comment characters:"));
-		myPanel.add(blockComField);
+		JPanel panel = new JPanel(new GridLayout(4, 2));
+		panel.add(new JLabel("Name:"));
+		panel.add(nameField);
+		panel.add(new JLabel("Extensions:"));
+		panel.add(extensionsField);
+		panel.add(new JLabel("Line comment characters:"));
+		panel.add(lineComField);
+		panel.add(new JLabel("Block comment characters:"));
+		panel.add(blockComField);
 
-		int result = JOptionPane.showConfirmDialog(null, myPanel,
+		int result = JOptionPane.showConfirmDialog(null, panel,
 				"Please Enter X and Y Values", JOptionPane.OK_CANCEL_OPTION);
 		if (result == JOptionPane.OK_OPTION) {
-			System.out.println("x value: " + nameField.getText());
-			System.out.println("y value: " + extensionsField.getText());
+			
+			// Create String objects that can be manipulated, and remove all whitespace characters
+			String nameText = nameField.getText().replaceAll("\\s+","");
+			String extsText = extensionsField.getText().replaceAll("\\s+","");
+			String lncmText = lineComField.getText().replaceAll("\\s+","");
+			String blcmText = blockComField.getText();
+			
+			String[] extensions = extsText.split(",");
+			String[] lineComChars = lncmText.split(",");
+			String[][] blockComChars = {blcmText.split(" ")};
+			
+			/*
+			System.out.println("Name: " + nameText);
+			System.out.println("Extensions: " + Arrays.toString(extensions));
+			System.out.println("Line comment characters: " + Arrays.toString(lineComChars));
+			System.out.println("Block comment characters: " + Arrays.toString(blockComChars));
+			*/
+			
+			// TODO Data validation
+			settings.availableLangs.addLanguage(new Language(nameText, extensions, lineComChars, blockComChars));
 		}
 	}
 
