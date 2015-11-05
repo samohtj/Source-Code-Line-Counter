32281177b6fc3cf4884b7564d73271a4f0cce5d4
Jonathan Thomas <thomas.jonathan.c@gmail.com>
2015-11-05 15:30:00 -0500

    Adds back .classpath, adds package-info.java

---
+++ src/linecounter/userinterface/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * The {@code userinterface} package contains classes for presenting the user interface to the
+ * user. Most of them are self-contained Swing components that are assembled into the final
+ * interface window.
+ */
+package linecounter.userinterface;
